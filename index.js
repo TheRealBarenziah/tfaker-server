34401799eb2aec88abc0e4e7285959f423b32a19
Barenziah <lunasecrete@gmail.com>
2019-09-29 14:58:14 +0200

    Fix stuff

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var port = 9615;
 
 http.createServer(function (request, response) {
   try {
+    console.log(request)
     var requestUrl = url.parse(request.url);
 
     // need to use path.normalize so people can't access directories underneath baseDirectory
