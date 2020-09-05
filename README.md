89a518338554fb8957de5a1b1c273b64bcc91e42
Barenziah <lunasecrete@gmail.com>
2020-09-05 12:37:33 +0200

    Add: Dockerfile, numerous improvements
    - Add valid Dockerfile using nvm & the ol' reliable node v0.10.48
    - Add nodejs & docker instruction in README.md
    - Add favicon
    - Add 'Dragon' field in index.html
    - Add 'dragon()' method in main.js
    - Fix style: as we don't support Netscape8 and IE4 yet, I took the
    liberty to add some flexboxes for a nicer layout
    - Fix footer text
    - Add robots.txt

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # tfaker-server
-In your GNU/Linux terminal :
+### In terminal, with nodejs on your system :
 ```bash
 git clone git@github.com:TheRealBarenziah/tfaker-server.git && \
 cd ./tfaker-server && \
 node index.js
 
-
 listening on port 9615
 ```
+[Go to localhost:9615 with your browser](http://0.0.0.0:9615)
+
+### In terminal, with Docker on your system :
+```bash
+docker image build -t tfaker-server:v1 -f docker/Dockerfile .
+
+docker container run --rm --name tfakerserver -d -p 9615:9615 tfaker-server:v1
+```
+[Go to localhost:9615 with your browser](http://0.0.0.0:9615)
--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
-var baseDirectory = __dirname;   // or whatever base directory you want
+var baseDirectory = __dirname;
 
-var port = 9615;
+// is there any PORT environment variable that is also a valid integer? Use that. Else use 9615
+var port = ( process.env.PORT && !isNaN(parseInt( process.env.PORT, 10))) ? process.env.PORT : 9615;
 
 http.createServer(function (request, response) {
   try {
     var requestUrl = url.parse(request.url);
+
+    // Did you just request root? Guess you actually expected /index.html. Else nevermind
+    requestUrl.pathname === "/" ? requestUrl.pathname = "/index.html" : null;
+
     // need to use path.normalize so people can't access directories underneath baseDirectory
     var fsPath = baseDirectory + path.normalize(requestUrl.pathname);
 
+    // create stream of matching file
     var fileStream = fs.createReadStream(fsPath);
+
     fileStream.pipe(response);
     fileStream.on('open', function () {
       response.writeHead(200);
@@ -24,8 +31,8 @@ http.createServer(function (request, response) {
   } catch (e) {
     response.writeHead(500);
     response.end();
-    console.log(e.stack);
+    console.error(e.stack);
   };
 }).listen(port);
 
-console.log("listening on port " + port);
+console.log("Listening on port " + port);
